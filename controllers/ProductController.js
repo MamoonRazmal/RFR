17311d28a86772f766544bf5e7e2af9eb6a19d65
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-11-01 20:36:05 +0100

    product controller photo line 99 changed

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -96,7 +96,7 @@ export const productPhotoController = async (req, res) => {
   try {
     const product = await productModel.findById(req.params.pid).select("photo");
     if (product.photo.data) {
-      res.set("content-type", product.photo.contentType);
+      res.set("Content-type", product.photo.contentType);
       return res.status(200).send(product.photo.data);
     }
   } catch (error) {
